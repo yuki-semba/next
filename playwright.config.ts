0b939f64660b9263f388669ee66b9cab9da8945c
yuki_senba@kyoso.co.jp <yuki_senba@kyoso.co.jp>
2026-06-17 15:57:53 +0900

    テストケース追加

--- playwright.config.ts
+++ playwright.config.ts
@@ -35,8 +35,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   // reporter: 'html',
   reporter: [
-    ['html'], 
-    ['./scripts/coverage-link-reporter.js']
+    ['html'],  ['list']
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
